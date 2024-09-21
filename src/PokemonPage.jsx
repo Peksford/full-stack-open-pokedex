a698e7a56be877bbd90a429354e857f7cac25ded
Peksford <juhapekkaj.virtanen@gmail.com>
2024-09-21 22:18:10 +0300

    fixing tests

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -9,6 +9,8 @@ const formatName = (nameWithDash) => nameWithDash.replace('-', ' ');
 
 const PokemonPage = ({ previous, next }) => {
   const { name } = useParams();
+  console.log('Previous', previous);
+  console.log('Next', next);
   const {
     data: pokemon,
     error,
@@ -40,7 +42,7 @@ const PokemonPage = ({ previous, next }) => {
       <div className="links">
         {previous && <Link to={`/pokemon/${previous.name}`}>Previous</Link>}
         <Link to="/">Home</Link>
-        {next && <Link to={`/pokemon/${previous.name}`}>Next</Link>}
+        {next && <Link to={`/pokemon/${next.name}`}>Next</Link>}
       </div>
       <div className={`pokemon-page pokemon-type-${type.name}`}>
         <div
